07d2b6acf6502245b19883cdf55202da0e0f3d7d
Rhuan Nascimento Ferreria <rhuan2414@gmail.com>
2023-10-31 16:07:37 -0300

    Finaliza

--- Projeto 1/python_etl/etl.py
+++ Projeto 1/python_etl/etl.py
@@ -2,7 +2,6 @@ import pymongo
 import psycopg2
 import csv
 
-
 #informações do mongo DB
 mongo_host = "mongodb"  
 mongo_port = 27017
@@ -12,7 +11,7 @@ mongo_db_name = "ecommerce"
 
 postgres_host = "postgres"  
 postgres_port = 5432
-postgres_db_name = "postgre"
+postgres_db_name = "pb_dw"
 postgres_user = "root"
 postgres_password = "root"
 
@@ -177,6 +176,38 @@ def insert_data_into_payment_dimension(data):
     except psycopg2.Error as e:
         print(f"Erro ao inserir dados na tabela Dimensão de Pagamento: {e}")
 
+# Função para preencher a coluna `order_review` com os dados coletados do MongoDB
+def update_order_review_data(reviews):
+    try:
+        cursor = postgres_conn.cursor()
+        update_query = """
+        UPDATE dimensao_pedido
+        SET order_review = %s
+        WHERE order_id = %s;
+        """
+        for review in reviews:
+            review_data = review["review_score"]
+            cursor.execute(update_query, (review_data, review["order_id"]))
+        postgres_conn.commit()
+        cursor.close()
+        print("Dados da coluna 'order_review' atualizados na tabela Dimensão do Pedido com 'review_score'.")
+    except psycopg2.Error as e:
+        print(f"Erro ao atualizar os dados da coluna 'order_review' com 'review_score': {e}")
+
+# Função para inserir dados na tabela Dimensão de Order Item
+def insert_data_into_order_item_dimension(data):
+    try:
+        cursor = postgres_conn.cursor()
+        insert_query = """
+        INSERT INTO dimensao_order_item (order_id, product_id, seller_id, shipping_limit_date, price, freight_value)
+        VALUES (%(order_id)s, %(product_id)s, %(seller_id)s, %(shipping_limit_date)s, %(price)s, %(freight_value)s);
+        """
+        cursor.executemany(insert_query, data)
+        postgres_conn.commit()
+        cursor.close()
+        print(f"{len(data)} registros inseridos na tabela Dimensão Order Item.")
+    except psycopg2.Error as e:
+        print(f"Erro ao inserir dados na tabela Dimensão Order Item: {e}")
 
 #dados do csv
 customers = read_csv_to_dict_list('/python_etl/input/olist_customers_dataset.csv') #Endereço do cliente
@@ -203,10 +234,10 @@ CREATE TABLE IF NOT EXISTS dimensao_pedido (
 """
 
 # Criara tabela Dimensão do Pedido
-#execute_postegres_query(create_order_dimension_table_query)
+execute_postegres_query(create_order_dimension_table_query)
 
 # Insere os dados das tabelas de origem na tabela Dimensão do Pedido
-#insert_data_into_order_dimension(orders)
+insert_data_into_order_dimension(orders)
 
 # Define a query para criar a tabela Dimensão do Cliente
 create_customer_dimension_table_query = """
@@ -220,10 +251,10 @@ CREATE TABLE IF NOT EXISTS dimensao_cliente (
 """
 
 # Criara tabela Dimensão do Pedido
-#execute_postegres_query(create_customer_dimension_table_query)
+execute_postegres_query(create_customer_dimension_table_query)
 
 # Inserir dados na tabela Dimensão do Cliente
-#insert_data_into_customer_dimension(customers)
+insert_data_into_customer_dimension(customers)
 
 # Define a query para criar a tabela Dimensão do Produto
 create_product_dimension_table_query = """
@@ -241,10 +272,10 @@ CREATE TABLE IF NOT EXISTS dimensao_produto (
 """
 
 # Criara tabela Dimensão do Produto
-#execute_postegres_query(create_product_dimension_table_query)
+execute_postegres_query(create_product_dimension_table_query)
 
 # Inserir dados na tabela Dimensão do Produto
-#insert_data_into_product_dimension(products)
+insert_data_into_product_dimension(products)
 
 # Define a query para criar a tabela Dimensão do Pagamento
 create_payment_dimension_table_query = """
@@ -263,8 +294,73 @@ execute_postegres_query(create_payment_dimension_table_query)
 # Inserir dados na tabela Dimensão do Pagamento
 insert_data_into_payment_dimension(order_payments)
 
+# Define a query para criar a coluna order_review na dimensao pedido 
+create_order_review_column_in_order_table_query = """
+    ALTER TABLE dimensao_pedido
+    ADD COLUMN order_review INTEGER;
+"""
+
+# Adicionar a coluna `order_review` à tabela de dimensão "pedido"
+execute_postegres_query(create_order_review_column_in_order_table_query)
+
+# Preencher a coluna `order_review` com a coluna `review_score` do MongoDB
+update_order_review_data(mongo_data)
+
+
+#Define a query para criar tabela dimensao order item
+create_table_dimesion_order_item_query = """
+CREATE TABLE IF NOT EXISTS dimensao_order_item (
+    order_item_id SERIAL PRIMARY KEY,
+    order_id VARCHAR,
+    product_id VARCHAR,
+    seller_id VARCHAR,
+    shipping_limit_date TIMESTAMP,
+    price NUMERIC,
+    freight_value NUMERIC
+);
+"""
+
+#Cria tabela dimensao order item 
+execute_postegres_query(create_table_dimesion_order_item_query)
+
+#Insere dados em dimensao_order_item
+insert_data_into_order_item_dimension(order_item)
+
+# Define a query para criar tabela fato de vendas
+create_fact_order_table_query = """
+CREATE TABLE IF NOT EXISTS fato_vendas (
+    sale_id SERIAL PRIMARY KEY,
+    order_id VARCHAR,
+    product_id VARCHAR,
+    customer_id VARCHAR,
+    payment_type VARCHAR,
+    order_date TIMESTAMP,
+    review_score INTEGER
+);
+"""
+
+# Criara tabela Fato de Vendas
+execute_postegres_query(create_fact_order_table_query)
+
+insert_query = """
+    INSERT INTO fato_vendas (order_id, product_id, customer_id, payment_type, order_date, review_score)
+    SELECT
+        dp.order_id,
+        doi.product_id,
+        dc.customer_id,
+        dpay.payment_type,
+        dp.order_purchase_timestamp,
+        dp.order_review
+    FROM dimensao_pedido dp
+    JOIN dimensao_cliente dc ON dp.customer_id = dc.customer_id
+    JOIN dimensao_pagamento dpay ON dp.order_id = dpay.order_id
+    JOIN dimensao_order_item doi ON dp.order_id = doi.order_id;    
+"""
+
+execute_postegres_query(insert_query)
+
 select_query = """
-SELECT * FROM dimensao_pagamento;
+SELECT * FROM fato_vendas;
 """
 
 try:
